c26dda67bb81ecb95eb6584b1e8f0fdaeb7cff0c
alfred161 <149138275+alfred161@users.noreply.github.com>
2024-04-05 14:08:02 +0800

    Task 1 and 2 mark up and JS

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,12 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
 let gallery = document.querySelector(".gallery");
+const basicLightboxInstance = null;
 
 galleryItems.forEach((item) => {
-	let li = `
+  let li = `
     <li class="gallery__item">
-        <a class="gallery__link" href="large-image.jpg">
+      <a class="gallery__link">
         <img
             class="gallery__image"
             src="${item.preview}"
@@ -16,5 +16,32 @@ galleryItems.forEach((item) => {
         </a>
     </li>
     `;
-	gallery.innerHTML += li;
+  gallery.innerHTML += li;
+});
+
+gallery.addEventListener("click", (e) => {
+  if (e.target.tagName === "IMG") {
+    const basicLightboxInstance = basicLightbox.create(`
+    <div class="modal">
+      <img
+        src="${e.target.getAttribute("data-source")}"
+        alt="${e.target.getAttribute("alt")}"
+      />
+    </div>
+  `);
+
+    basicLightboxInstance.show();
+
+    document.querySelector(".basicLightbox").addEventListener("click", () => {
+      if (basicLightboxInstance.visible()) {
+        basicLightboxInstance.close();
+      }
+    });
+
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "Escape" && basicLightboxInstance.visible()) {
+        basicLightboxInstance.close();
+      }
+    });
+  }
 });
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+let gallery = document.querySelector(".gallery");
+const basicLightboxInstance = null;
+
+galleryItems.forEach((item) => {
+  let li = `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+          title="${item.description}"
+      />
+      </a>
+  </li>
+      `;
+  gallery.innerHTML += li;
+});
+
+let galleryImage = new SimpleLightbox(".gallery__link", {});
+
+gallery.addEventListener("click", (e) => {
+  if (e.target.tagName === "IMG") {
+    galleryImage.open();
+  }
+});
